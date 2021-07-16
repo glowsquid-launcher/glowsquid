5ce70983316488800649d1643af80ae30d55818e
redcreeper14385 <mounderfod@gmail.com>
2021-07-16 17:57:59 +0100

    fix: update modrinth adapter to use the new spec

--- adapters/packages/adapter-modrinth/src/Adapter/Functions/getAllVersions.ts
+++ adapters/packages/adapter-modrinth/src/Adapter/Functions/getAllVersions.ts
@@ -6,6 +6,6 @@ export const getAllVersions = async (modId: string): Promise<ModVersion[]> => {
   const mod = await getMod(modId)
 
   return await Promise.all(
-    mod.versions.map(async (modVer) => await getVersion(modVer)),
+    mod.versions.map(async (modVer) => await getVersion(modId, modVer)),
   )
 }
