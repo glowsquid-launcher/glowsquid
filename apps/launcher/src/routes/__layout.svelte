e21ba09ed37b4a5c3ccc09a54b358f8c218c4550
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2022-01-08 23:02:20 +0200

    feat(launcher): :lipstick: get carbon-components-svelte to work

---
+++ apps/launcher/src/routes/__layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import 'carbon-components-svelte/css/g100.css';
+</script>
+
+<slot />
--- apps/launcher/svelte.config.js
+++ apps/launcher/svelte.config.js
@@ -1,15 +1,52 @@
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static'
+import { elements, icons, optimizeCss, optimizeImports, pictograms } from 'carbon-preprocess-svelte'
+import preprocess from 'svelte-preprocess'
+import WindiCSS from 'vite-plugin-windicss'
+
+const production = process.env.NODE_ENV === 'production'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+	// so many preprocessors
+	preprocess: [
+		preprocess({}),
+		optimizeImports(),
+		icons(),
+		pictograms(),
+		elements({
+			theme: 'all'
+		})
+	],
 
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-  },
-};
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			// for an SPA / dynamic app
+			fallback: 'index.html'
+		}),
 
-export default config;
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		ssr: false,
+
+		vite: {
+			optimizeDeps: {
+				include: ['@carbon/charts']
+			},
+			ssr: {
+				noExternal: ['@tauri-apps/api', production && '@carbon/charts'].filter(Boolean)
+			},
+			plugins: [WindiCSS(), production && optimizeCss()],
+			esbuild: {
+				target: ['esnext', 'chrome89', 'safari15.1', 'edge89', 'firefox89']
+			},
+			build: {
+				target: ['esnext', 'chrome89', 'safari15.1', 'edge89', 'firefox89']
+			}
+		}
+	}
+}
+
+export default config
