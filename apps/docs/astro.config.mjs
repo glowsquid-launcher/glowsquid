85142bedf5f8d109818a41e02697eec71330d091
Suyashtnt <suyashtnt@gmail.com>
2022-04-02 20:16:10 +0200

    dep update

--- apps/docs/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
-
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  dist: '../../dist/apps/docs',
-  renderers: ["@astrojs/renderer-svelte"]
-});
--- apps/tnt/src/lib/invoke.ts
+++ apps/tnt/src/lib/invoke.ts
@@ -5,13 +5,24 @@ import type { Instance } from './instances'
 type FunctionType = (arg: Record<string, unknown>) => unknown
 
 interface InvokeMap {
-	'get_instance'(args: { name: string }): Instance
-	'get_app_path'(): string
+  'get_instance'(args: { name: string }): Instance
+  /**
+   * gets `amount` or all instances
+   *
+   *
+   * @param amount The amount of instances. Sorted by recently used
+   * */
+  'get_instances'(args: { amount?: number }): Instance[]
+  'get_app_path'(): string
 }
 
+/**
+ * invokes a command except its fully typed
+ *
+*/
 export function invoke<T extends InvokeMap, C extends Object.SelectKeys<T, FunctionType>>(
-	c: C,
-	args: Parameters<T[C]>[0]
+  c: C,
+  args: Parameters<T[C]>[0]
 ): Promise<ReturnType<T[C]>> {
-	return tauriInvoke(c, args)
+  return tauriInvoke(c, args)
 }
