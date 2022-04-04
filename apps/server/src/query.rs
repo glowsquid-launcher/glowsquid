6d74b33405240f1bc4f9e9d60a9e004585b6f89b
Suyashtnt <suyashtnt@gmail.com>
2022-04-04 16:32:02 +0200

    replace add with is up

--- apps/server/src/query.rs
+++ apps/server/src/query.rs
@@ -4,8 +4,8 @@ pub struct Query;
 
 #[Object]
 impl Query {
-    /// Returns the sum of a and b
-    async fn add(&self, a: i32, b: i32) -> i32 {
-        a + b
+    /// is the server up
+    async fn is_up(&self) -> bool {
+        true
     }
 }
