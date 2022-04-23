a86bda76a78217187d1589f43d806d3c470b42f2
gralp <62028969+gralp-1@users.noreply.github.com>
2022-04-23 14:30:32 +0100

    Setup website

---
+++ apps/website/src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<div class="index-page">
+	<div class="top-section">
+		<h1>Glowsquid launcher</h1>
+	</div>
+</div>
+
+<style lang="sass">
+	.index-page
+		display: flex
+		flex-direction: column
+</style>
