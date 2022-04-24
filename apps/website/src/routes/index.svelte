aa93f98075496a8a17db436fa7c67b3bf2dfc8d6
gralp <62028969+gralp-1@users.noreply.github.com>
2022-04-24 14:04:53 +0100

    Add some content and basic style to glowsquid website

--- apps/website/src/routes/index.svelte
+++ apps/website/src/routes/index.svelte
@@ -1,8 +1,69 @@
 <script>
 </script>
 
-<div flex="~ direction=column">
+<div class="index-page" flex="~ direction=column">
 	<div class="top-section">
+		<!-- TODO: Find a background image for this section to make it look nice, then find a font colour for it -->
 		<h1>Glowsquid launcher</h1>
 	</div>
+	<div class="middle-section">
+		<div class="point">
+			<h1>Fast</h1>
+			<p>Built on rust and svelte.</p>
+		</div>
+		<div class="seperator" />
+		<div class="point">
+			<h1>Open source</h1>
+			<p>Free, forever.</p>
+		</div>
+		<div class="seperator" />
+		<div class="point">
+			<h1>Useful</h1>
+			<p>Helpful defaults and easy modpack installation</p>
+		</div>
+	</div>
 </div>
+
+<style>
+	.index-page {
+		font-family: 'Varela', sans-serif;
+	}
+	.top-section {
+		color: black;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 10px;
+		margin-bottom: 10px;
+		height: calc(100vh / 3 * 2);
+		text-align: center;
+	}
+	.middle-section {
+		color: rgb(186, 186, 186);
+		background-color: rgb(61, 61, 61);
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		align-items: center;
+		flex-wrap: wrap;
+		padding: 10px;
+		height: calc(100vh / 3);
+	}
+	.middle-section .point {
+		/* Align all text to the centre */
+		text-align: center;
+		width: 15vw;
+		height: 20vh;
+		border-radius: 10px;
+		margin: 10px;
+		padding: 10px;
+	}
+	.middle-section .point h1 {
+		font-size: 2em;
+		margin-bottom: 10px;
+	}
+	.middle-section .point p {
+		font-size: 1em;
+	}
+</style>
