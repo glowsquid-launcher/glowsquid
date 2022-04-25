36f8ea4d776271a981e39e1e716a09a4712fc0dd
Suyashtnt <suyashtnt@gmail.com>
2022-04-25 18:52:52 +0200

    docs(ui): add docs on setTheme

--- libs/glow-ui/src/lib/helpers.ts
+++ libs/glow-ui/src/lib/helpers.ts
@@ -1,5 +1,12 @@
 import { browser } from '$app/env';
 
+/**
+ * sets the theme of the app
+ *
+ * @param {string} theme The theam. As of now can be `light` or `dark`
+ *
+ * @remarks All this does is edit the class of the main document. Its more of a utility function than some complex thing
+ **/
 export function setTheme(theme: 'dark' | 'light') {
   if (browser) document.documentElement.className = theme;
 }
