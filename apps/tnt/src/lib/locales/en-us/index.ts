11c7b5e1739d4c1c8e83e09ae65714a0ec92d1b6
Suyashtnt <suyashtnt@gmail.com>
2022-05-01 00:00:30 +0200

    fix(tnt): :art: fix some issues

--- apps/tnt/src/lib/locales/en-us/index.ts
+++ apps/tnt/src/lib/locales/en-us/index.ts
@@ -1,9 +1,9 @@
 import type { Translation } from '../i18n-types'
 
 // translations for the main UI in english (simplified)
-const us: Translation = {
+const en_us: Translation = {
 	// this is an example Translation, just rename or delete this folder if you want
 	HI: 'Hello {name}! Please leave a star if you like this project: https://github.com/glowsquid-launcher/glowsquid',
 }
 
-export default us
+export default en_us
--- apps/tnt/src/lib/util.ts
+++ apps/tnt/src/lib/util.ts
@@ -4,7 +4,10 @@ import { setLocale } from "$locales/i18n-svelte";
 import { baseLocale, locales} from "$locales/i18n-util";
 import { loadLocale } from "$locales/i18n-util.sync";
 
-export const refreshLocales = () => {
+/**
+ * refreshes the current locale and loads whatever new things you wanted it to load
+ */
+export const refreshLocales = (): void => {
 if (browser) {
     const currentLocale = detectLocale(
       baseLocale,
