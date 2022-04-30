5671531500f33a2f6c884195183785e78b354fc4
Restyled.io <commits@restyled.io>
2022-04-30 21:57:05 +0000

    Restyled by clang-format

--- libs/glow-ui/svelte.config.js
+++ libs/glow-ui/svelte.config.js
@@ -5,10 +5,10 @@ import preprocess from 'svelte-preprocess';
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess : preprocess(),
 
-  kit: {
-    adapter: adapter(),
+  kit : {
+    adapter : adapter(),
   },
 };
 
