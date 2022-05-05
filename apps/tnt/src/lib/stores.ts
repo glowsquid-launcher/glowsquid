173ec939c92731ca3e8ed948f056ba2a3c6f76b4
Suyashtnt <suyashtnt@gmail.com>
2022-05-05 19:37:30 +0200

    fix(website): fix minor issues

--- apps/tnt/src/lib/stores.ts
+++ apps/tnt/src/lib/stores.ts
@@ -1,6 +1,7 @@
 import { browser } from '$app/env'
 import type { Writable } from 'svelte/store'
 import { get, writable } from 'svelte/store'
+import type { Union } from 'ts-toolbelt'
 
 /**
  * creates a store that is persisted in localStorage
@@ -12,10 +13,10 @@ const storage = <T>(key: string, initValue: T): Writable<T> => {
   if (!browser) return store
 
   const storedValueStr = localStorage.getItem(key)
-  if (storedValueStr !== null) store.set(JSON.parse(storedValueStr))
+  if (storedValueStr !== null) store.set(JSON.parse(storedValueStr));
 
   store.subscribe((val) => {
-    if ([null, undefined].includes(val as any)) {
+    if (val === null || val === undefined) {
       localStorage.removeItem(key)
     } else {
       localStorage.setItem(key, JSON.stringify(val))
--- libs/glow-ui/src/lib/helpers.ts
+++ libs/glow-ui/src/lib/helpers.ts
@@ -7,6 +7,6 @@ import { browser } from '$app/env'
  *
  * @remarks All this does is edit the class of the main document. Its more of a utility function than some complex thing
  **/
-export function setTheme (theme: 'dark' | 'light') {
+export function setTheme (theme: 'dark' | 'light'): void {
   if (browser) document.documentElement.className = theme
 }
