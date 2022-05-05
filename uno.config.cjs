e3773cc8c315924b23af9bdbcb08f223cc3e826e
Suyashtnt <suyashtnt@gmail.com>
2022-05-05 18:07:35 +0200

    feat: :lipstick: add different variations of variant colours
    e.g error now has `error-highlight` and `error-active`

--- uno.config.cjs
+++ uno.config.cjs
@@ -26,16 +26,39 @@ module.exports = {
   theme: {
     // even though I use english(traditional) spelling I'll use english(simplified) as thats what css uses
     colors: {
+      // MAIN COLOURS
       primary: 'var(--color-primary)',
       secondary: 'var(--color-secondary)',
       highlight: 'var(--color-highlight)',
       active: 'var(--color-active)',
       disabled: 'var(--color-disabled)',
+      
+      // ACCENT/UI COMPONENT VARIATION COLOURS
+
+      // error
       error: 'var(--color-error)',
+      'error-highlight': 'var(--color-error-highlight)',
+      'error-active': 'var(--color-error-active)',
+
+      // warning
       warning: 'var(--color-warning)',
+      'warning-highlight': 'var(--color-warning-highlight)',
+      'warning-active': 'var(--color-warning-active)',
+      
+      // success
       success: 'var(--color-success)',
+      'success-highlight': 'var(--color-success-highlight)',
+      'success-active': 'var(--color-success-active)',
+      
+      // danger
       danger: 'var(--color-danger)',
-      link: 'var(--color-link)'
+      'danger-highlight': 'var(--color-danger-highlight)',
+      'danger-active': 'var(--color-danger-active)',
+      
+      // link
+      link: 'var(--color-link)',
+      'link-highlight': 'var(--color-link-highlight)',
+      'link-active': 'var(--color-link-active)',
     },
   },
 };
