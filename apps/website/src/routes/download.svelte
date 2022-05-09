aa6c424983aaebe9f037dc8ec45fba2811dd1c70
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2022-05-09 07:44:49 +0200

    docs(website): remove comment

--- apps/website/src/routes/download.svelte
+++ apps/website/src/routes/download.svelte
@@ -1,5 +1,4 @@
-<script>// Find what operating system the user is on
-import UAParser from 'ua-parser-js'
+<script lang="ts">import UAParser from 'ua-parser-js'
 const ua = new UAParser()
 // eslint-disable-next-line no-unused-vars
 const detectedOS = ua.getOS().name
