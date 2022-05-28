f006b7c1721f169f43d72138f9f9aee418a5475a
Suyashtnt <suyashtnt@gmail.com>
2022-05-28 22:45:30 +0200

    feat(tnt): update animation timings

--- apps/tnt/src/lib/animations.ts
+++ apps/tnt/src/lib/animations.ts
@@ -1,3 +1,3 @@
 // the animation delay for all animations/transitions
-export const animationTime = 500 as const
-export const transitionDelay = 800 as const
+export const animationTime = 300 as const
+export const transitionDelay = 500 as const
