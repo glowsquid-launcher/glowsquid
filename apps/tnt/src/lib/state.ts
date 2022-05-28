9e27a5a6302dc17b471282ffcd3cb10e6747e6eb
Suyashtnt <suyashtnt@gmail.com>
2022-05-28 22:44:58 +0200

    feat(tnt): redo state managment

--- apps/tnt/src/lib/state.ts
+++ apps/tnt/src/lib/state.ts
@@ -1,8 +1,12 @@
-import { writable, readable } from 'svelte/store'
+import { writable, derived } from 'svelte/store'
 
 const defaultState = {
   modals: {
     createInstance: false,
+  },
+  accounts: {
+    current: null as string | null,
+    list: ['52ddf2f1-a59f-4a19-822f-a6157f705320'],
   },
 }
 const state = writable(defaultState)
@@ -14,8 +18,17 @@ export const toggleInstanceModal = () => {
   })
 }
 
-const exposedState = readable<typeof defaultState>(defaultState, (set) =>
-  state.subscribe((v) => set(v))
-)
+export const updateCurrentAccount = (account: string) => {
+  state.update((s) => {
+    if (s.accounts.list.includes(account)) {
+      s.accounts.current = account
+      return s
+    } else {
+      throw new Error('Account not found')
+    }
+  })
+}
+
+const exposedState = derived(state, (v) => v, defaultState)
 
 export default exposedState
