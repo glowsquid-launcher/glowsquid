3c09a27744e996fe978964014e6421be3d30f9ad
Suyashtnt <suyashtnt@gmail.com>
2022-06-09 18:23:54 +0200

    wip: wip

--- libs/glow-ui/src/lib/index.ts
+++ libs/glow-ui/src/lib/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from '$lib/components/button/Button.svelte'
 export { default as Dropdown } from '$lib/components/dropdown/Dropdown.svelte'
+export { default as Modal } from '$lib/components/modal/Modal.svelte'
 export * from './types'
 export * from '$lib/helpers'
