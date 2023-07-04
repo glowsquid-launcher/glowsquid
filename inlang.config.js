76f923af1cf5e09c0d93cc6c57c57ae1b2306233
Suyashtnt <suyashtnt@gmail.com>
2023-07-04 15:42:00 +0200

    wip: update i18n conf

--- inlang.config.js
+++ inlang.config.js
@@ -6,7 +6,11 @@ export async function defineConfig(env) {
         'https://cdn.jsdelivr.net/gh/ivanhofer/inlang-plugin-typesafe-i18n@2.3.0/dist/index.js'
     );
 
+    const {default: standardLintRules} = await env.$import(
+        'https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@2/dist/index.js'
+    );
+
     return {
-        plugins: [typesafeI18nPlugin({})]
+        plugins: [typesafeI18nPlugin({}), standardLintRules()]
     };
 }
