f8fcd65017b15dd8cd0ebb92b96b9f0ead5a2d79
Suyashtnt <suyashtnt@gmail.com>
2023-08-22 20:35:53 +0200

    wip: get client to build in right dir

--- svelte.config.js
+++ svelte.config.js
@@ -1,46 +1,48 @@
 import adapter from '@sveltejs/adapter-static';
-import {vitePreprocess} from '@sveltejs/kit/vite';
-import {preprocessMeltUI} from '@melt-ui/pp';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import { preprocessMeltUI } from '@melt-ui/pp';
 import sequence from 'svelte-sequential-preprocessor';
 
 const dir = "apps/frontend"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter({
-            fallback: 'index.html'
-        }),
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html',
+      pages: 'dist/apps/frontend',
+      assets: 'dist/apps/frontend',
+    }),
 
-        alias: {
-            $components: `${dir}/src/lib/components`,
-            $i18n: `${dir}/src/i18n/i18n-svelte`
-        },
-
-        files: {
-          assets: `${dir}/static`,
-          appTemplate: `${dir}/src/app.html`,
-          errorTemplate: `${dir}/src/error.html`,
-          hooks: {
-            client: `${dir}/src/hooks.client`,
-            server: `${dir}/src/hooks.server`,
-          },
-          lib: `${dir}/src/lib`,
-          routes: `${dir}/src/routes`,
-          params: `${dir}/src/params`,
-          serviceWorker: `${dir}/src/service-worker`,
-        },
-
-        outDir: `${dir}/.svelte-kit`,
-
-        typescript: {
-          config(cfg) {
-            // cfg.extends = "../../../tsconfig.base.json";
-          }
-        }
+    alias: {
+      $components: `${dir}/src/lib/components`,
+      $i18n: `${dir}/src/i18n/i18n-svelte`
     },
 
-    preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+    files: {
+      assets: `${dir}/static`,
+      appTemplate: `${dir}/src/app.html`,
+      errorTemplate: `${dir}/src/error.html`,
+      hooks: {
+        client: `${dir}/src/hooks.client`,
+        server: `${dir}/src/hooks.server`,
+      },
+      lib: `${dir}/src/lib`,
+      routes: `${dir}/src/routes`,
+      params: `${dir}/src/params`,
+      serviceWorker: `${dir}/src/service-worker`,
+    },
+
+    outDir: `${dir}/.svelte-kit`,
+
+    typescript: {
+      config(cfg) {
+        // cfg.extends = "../../../tsconfig.base.json";
+      }
+    }
+  },
+
+  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 };
 
 export default config;
